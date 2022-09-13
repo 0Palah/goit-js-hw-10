9dc1d7de6c32c8f2db56693339f300847e35e5c7
Oleg Palashynskyi <palah@ukr.net>
2022-09-13 17:38:28 +0300

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const searchInputEl = document.querySelector('#search-box');
@@ -11,34 +12,36 @@ console.log(searchInputEl);
 console.log(countryListEl);
 console.log(countryInfoEl);
 
-searchInputEl.addEventListener('input', evt => {
-  evt.preventDefault();
-  //   console.log(evt);
-  const countryName = evt.target.value.trim();
-  console.log(countryName);
-  if (countryName === '') {
-    return;
-  }
-  fetchCountries(countryName)
-    .then(data => {
-      console.log(data);
-    })
-    .catch(error => {
-      if (error.message === '404') {
-        alert('Таку країну не знайдено');
+searchInputEl.addEventListener(
+  'input',
+  debounce(evt => {
+    evt.preventDefault();
+    //   console.log(evt);
+    const countryName = evt.target.value.trim();
+    console.log(countryName);
+    if (countryName === '') {
+      return;
+    }
+    fetchCountries(countryName)
+      .then(data => {
+        console.log(data);
+        if (data.length > 10) {
+          Notiflix.Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (data.length > 2 && data.length < 9) {
+          console.log('2 - 9');
+        } else {
+          console.log(1);
+        }
+      })
+      .catch(error => {
+        if (error.message === '404') {
+          Notiflix.Notify.failure('Oops, there is no country with that name');
+          //   alert('Таку країну не знайдено');
 
-        //   weatherWrapperEl.innerHTML = '';
-      }
-    });
-});
-
-// function fetchCountries(countryName) {
-//   fetch(`https://restcountries.com/v3.1/name/${countryName}`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error(response.status);
-//       }
-//       return response.json();
-//     })
-
-// }
+          //   weatherWrapperEl.innerHTML = '';
+        }
+      });
+  }, 1000)
+);
