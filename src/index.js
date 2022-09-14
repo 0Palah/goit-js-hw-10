b346212b92b118bb2346853900bcad698a046087
Oleg Palashynskyi <palah@ukr.net>
2022-09-14 19:49:13 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,86 +2,94 @@ import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
+import foundCountry from './templates/foundCountry.hbs';
+import countryItems from './templates/countryItems.hbs';
 
 const DEBOUNCE_DELAY = 300;
 const searchInputEl = document.querySelector('#search-box');
 const countryListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
-// console.log(searchInputEl);
-// console.log(countryListEl);
-// console.log(countryInfoEl);
+searchInputEl.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
-searchInputEl.addEventListener(
-  'input',
-  debounce(evt => {
-    evt.preventDefault();
-    //   console.log(evt);
-    const countryName = evt.target.value.trim();
-    console.log(countryName);
-    if (countryName === '') {
-      return;
-    }
-    fetchCountries(countryName)
-      .then(data => {
-        // console.log(data);
-        if (data.length > 10) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
-        } else if (data.length > 1 && data.length < 10) {
-          console.log('2 - 9');
-          const createCountryPreview = data => {
-            data.map(({ name, flags: { svg } }) => {
-              console.log(name.official);
-              console.log(svg);
-            });
-            console.log(data);
-          };
-          createCountryPreview(data);
-        } else {
-          const createCountryCard = ({
-            name,
-            capital,
-            population,
-            flags: { svg },
-            languages,
-          }) => {
-            console.log(name.official);
-            console.log(capital[0]);
-            console.log(population);
-            console.log(svg);
-            console.log(Object.values(languages)[0]);
-          };
-          console.log(data);
+function onInput(evt) {
+  const countryName = evt.target.value.trim();
+  countryListEl.innerHTML = '';
+  countryInfoEl.innerHTML = '';
 
-          createCountryCard(data[0]);
-        }
-      })
-      .catch(error => {
-        if (error.message === '404') {
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-          //   alert('Таку країну не знайдено');
+  if (!countryInfoEl) {
+    return;
+  }
 
-          //   weatherWrapperEl.innerHTML = '';
-        }
-      });
-  }, 1000)
-);
+  fetchCountries(countryName)
+    .then(data => {
+      if (data.length > 10) {
+        return Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+      } else if (data.length > 1 && data.length <= 10) {
+        console.log(data);
+        return (countryListEl.innerHTML = countryItems(data));
+      } else {
+        const createCountryCard = data => {
+          countryInfoEl.innerHTML = foundCountry(data);
+        };
+        let { name, capital, population, flags, languages } = data[0];
+        let coyntryObj = {
+          name,
+          capital,
+          population,
+          flags,
+          lengCh: Object.values(languages).join(', '),
+        };
+        createCountryCard(coyntryObj);
+      }
+    })
+    .catch(error => {
+      if (error.message === '404') {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+      }
+    });
+}
 
-// <li>
-//   <img class="flag-preview" src="#" alt="flag" /><span
-//     class="country-name-preview"
-//   ></span>
-// </li>
-
-// <h2>
-//   <img class="flag-preview" src="#" alt="flag" /><span
-//     class="country-name-result"
-//   ></span>
-// </h2>
-// <ul>
-//   <li class="result-desc">Capital: <span class="text-desc"></span></li>
-//   <li class="result-desc">Population: <span class="text-desc"></span></li>
-//   <li class="result-desc">Languages: <span class="text-desc"></span></li>
-// </ul>
+// searchInputEl.addEventListener(
+//   'input',
+//   debounce(evt => {
+//     evt.preventDefault();
+//     //   console.log(evt);
+//     const countryName = evt.target.value.trim();
+//     if (countryName === '') {
+//       return;
+//     }
+//     fetchCountries(countryName)
+//       .then(data => {
+//         // console.log(data);
+//         if (data.length > 10) {
+//           return Notiflix.Notify.info(
+//             'Too many matches found. Please enter a more specific name.'
+//           );
+//         } else if (data.length > 1 && data.length <= 10) {
+//           return (countryListEl.innerHTML = countryItems(data));
+//         } else {
+//           const createCountryCard = data => {
+//             countryInfoEl.innerHTML = foundCountry(data);
+//           };
+//           let { name, capital, population, flags, languages } = data[0];
+//           let coyntryObj = {
+//             name,
+//             capital,
+//             population,
+//             flags,
+//             lengCh: Object.values(languages).join(', '),
+//           };
+//           console.log(coyntryObj);
+//           createCountryCard(coyntryObj);
+//         }
+//       })
+//       .catch(error => {
+//         if (error.message === '404') {
+//           Notiflix.Notify.failure('Oops, there is no country with that name');
+//         }
+//       });
+//   }, 1000)
+// );
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -3,11 +3,11 @@
 const BASE_URL = 'https://restcountries.com/v3.1/name/';
 const queryParm = '?fields=name,capital,population,flags,languages';
 export const fetchCountries = countryName => {
-  return fetch(`${BASE_URL}${countryName}${queryParm}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
+  return fetch(`${BASE_URL}${countryName}${queryParm}`).then(data => {
+    if (!data.ok) {
+      throw new Error(data.status);
     }
 
-    return response.json();
+    return data.json();
   });
 };
