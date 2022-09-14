2724ad94090326bbca4fc55e04e931347b046342
Oleg Palashynskyi <palah@ukr.net>
2022-09-14 22:10:42 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -49,6 +49,8 @@ function onInput(evt) {
     .catch(error => {
       if (error.message === '404') {
         Notiflix.Notify.failure('Oops, there is no country with that name');
+        countryListEl.innerHTML = '';
+        countryInfoEl.innerHTML = '';
       }
     });
 }
